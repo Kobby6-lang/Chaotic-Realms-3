9b460b1ca9d602751a2e5e1cf4f18caed604f7d0
Paul Sinnett <paul.sinnett@gmail.com>
2025-03-15 20:42:15 +0000

    fix new Unity input

--- Assets/Kwabena Assets/FinalCharacterController/Scripts/CollisionCamera.cs
+++ Assets/Kwabena Assets/FinalCharacterController/Scripts/CollisionCamera.cs
@@ -1,3 +1,4 @@
+using Kwabena.FinalCharacterController;
 using UnityEngine;
 
 public class CollisionCamera : MonoBehaviour
@@ -6,25 +7,48 @@ public class CollisionCamera : MonoBehaviour
     [SerializeField] Transform facing;
     public LayerMask mask;
     public float radius = 0.3f;
+    float distance;
     public float maxDistance = 5.0f;
     public float minDistance = 1.0f;
     public float height = 2;
     public float smoothTime = 0.1f;
-    public float sensitivity = 360;
+    public float tiltSensitivity = 90;
+    public float scrollSensitivity = 10;
     public float maxPitch = 75;
     public float minPitch = -75;
     float pitch = 0;
     Vector3 velocity;
+    PlayerControls playerControls;
 
     void Reset()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
+    void Awake()
+    {
+        playerControls = new PlayerControls();
+        distance = maxDistance;
+    }
+
+    void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
     void Update()
     {
-        pitch -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        Vector2 mouseInput = playerControls.PlayerLocomotionMap.Look.ReadValue<Vector2>();
+        pitch -= mouseInput.y * tiltSensitivity * Time.deltaTime;
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
+        float scrollInput = playerControls.ThirdPersonMap.ScrollCamera.ReadValue<float>();
+        distance += scrollInput * scrollSensitivity * Time.deltaTime * distance;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
         Vector3 direction = Quaternion.AngleAxis(pitch, facing.right) * -facing.forward;
         Vector3 targetPosition;
         if (Physics.CheckSphere(target.position, radius, mask.value))
@@ -35,7 +59,7 @@ public class CollisionCamera : MonoBehaviour
         {
             if (hit.distance > minDistance)
             {
-                targetPosition = target.position + direction * hit.distance;
+                targetPosition = target.position + direction * Mathf.Min(hit.distance, distance);
             }
             else
             {
@@ -44,7 +68,7 @@ public class CollisionCamera : MonoBehaviour
         }
         else
         {
-            targetPosition = target.position + direction * maxDistance;
+            targetPosition = target.position + direction * distance;
         }
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
         transform.LookAt(transform.position - direction);
