986d2bab3df4e27e66d0de45b6709644b180408c
Kobby6-lang <koseitutu43@gmail.com>
2025-04-09 18:33:56 +0100

    ???

---
+++ Assets/Kwabena Assets/FinalCharacterController/UI Scripts/GameSettings.cs
@@ -0,0 +1,10 @@
+public class GameSettings
+{
+    public bool fullscreen;
+    public int textureQuality;
+    public int antialiasing;
+    public int vSync;
+    public int resolutionIndex;
+    public float musicVolume;
+    public float graphicOutline;
+}
--- Assets/Kwabena Assets/FinalCharacterController/UI Scripts/SettingsMenu.cs
+++ Assets/Kwabena Assets/FinalCharacterController/UI Scripts/SettingsMenu.cs
@@ -2,9 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
+using TMPro; // Import the TextMesh Pro namespace
+using System.IO;
 
-public class SettingsMenu : MonoBehaviour
+public class SettingsManager : MonoBehaviour
 {
+    public Toggle fullScreenToggle; // UI toggle remains unchanged
+    public TMP_Dropdown resolutionDropdown; // TextMesh Pro dropdown
+    public TMP_Dropdown textureQualityDropdown; // TextMesh Pro dropdown
+    public TMP_Dropdown antialiasingDropdown; // TextMesh Pro dropdown
+    public TMP_Dropdown vSyncDropdown; // TextMesh Pro dropdown
+    public Slider musicVolumeSlider; // Slider remains unchanged
+    public Button applyButton;
+
+    public AudioSource musicSource;
+    public Resolution[] resolutions;
+    public GameSettings gameSettings;
+
+    private void OnEnable()
+    {
+        gameSettings = new GameSettings();
+
+        fullScreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+        antialiasingDropdown.onValueChanged.AddListener(delegate { OnAntialiasingChange(); });
+        vSyncDropdown.onValueChanged.AddListener(delegate { OnVSyncChange(); });
+        musicVolumeSlider.onValueChanged.AddListener(delegate { OnMusicVolumeChange(); });
+        applyButton.onClick.AddListener(delegate { OnApplyButtonClick(); });
+
+        resolutions = Screen.resolutions;
+        foreach(Resolution resolution in resolutions) 
+        {
+            resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(resolution.ToString()));
+        }
+
+        LoadSettings();
+    }
     public Material outline;
     LocalKeyword outlineEffect;
 
@@ -12,18 +47,61 @@ public class SettingsMenu : MonoBehaviour
     {
         outlineEffect = new LocalKeyword(outline.shader, "_OVERLAY");
     }
-    public void SetFullscreen(bool isFullScreen) 
+    public void OnFullscreenToggle()
     {
-        Screen.fullScreen = isFullScreen;
+       gameSettings.fullscreen = Screen.fullScreen = fullScreenToggle.isOn;
+    }
+    public void OnResolutionChange() 
+    {
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
     }
 
-    public void SetQuality(int qualityIndex) 
+    public void OnTextureQualityChange() 
     {
-        Debug.Log("Setting quality level to: " + qualityIndex);
-        QualitySettings.SetQualityLevel(qualityIndex);
+        QualitySettings.globalTextureMipmapLimit = gameSettings.textureQuality = resolutionDropdown.value;
     }
 
-    public void SetGraphicsOutline(bool enable)
+    public void OnAntialiasingChange() 
+    {
+        QualitySettings.antiAliasing = gameSettings.antialiasing = (int)Mathf.Pow(2, antialiasingDropdown.value);
+    }
+
+    public void OnVSyncChange() 
+    {
+        QualitySettings.vSyncCount = gameSettings.vSync = vSyncDropdown.value;
+    }
+
+    public void OnMusicVolumeChange() 
+    {
+        musicSource.volume = gameSettings.musicVolume = musicVolumeSlider.value;
+    }
+
+    public void OnApplyButtonClick() 
+    {
+        SaveSettings();
+    }
+
+    public void SaveSettings() 
+    {
+        string jsonData = JsonUtility.ToJson(gameSettings, true);
+        File.WriteAllText(Application.persistentDataPath + "/gamesettings.json", jsonData);
+    }
+
+    public void LoadSettings() 
+    {
+        gameSettings = JsonUtility.FromJson<GameSettings>(File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
+
+        musicVolumeSlider.value = gameSettings.musicVolume;
+        antialiasingDropdown.value = gameSettings.antialiasing;
+        vSyncDropdown.value = gameSettings.vSync;
+        textureQualityDropdown.value = gameSettings.textureQuality;
+        resolutionDropdown.value = gameSettings.resolutionIndex;
+        fullScreenToggle.isOn = gameSettings.fullscreen;
+
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void OnGraphicsOutlineToggle(bool enable)
     {
         outline.SetKeyword(outlineEffect, enable);
     }
